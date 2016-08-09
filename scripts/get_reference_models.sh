2045b08a8e3f8b898ca838ba883e6350bff5df45
yjxiong <xy012@ie.cuhk.edu.hk>
2016-08-09 10:35:53 +0800

    fix hmdb rgb reference download

--- scripts/get_reference_models.sh
+++ scripts/get_reference_models.sh
@@ -11,9 +11,9 @@ wget -O models/ucf101_split_2_tsn_flow_reference_bn_inception.caffemodel http://
 wget -O models/ucf101_split_3_tsn_flow_reference_bn_inception.caffemodel http://mmlab.siat.ac.cn/tsn_model/ucf101_split_3_tsn_flow_reference_bn_inception.caffemodel.v5
 
 # hmdb51 rgb models, 3 splits
-wget -O models/hmdb51_split_1_tsn_rgb_reference_bn_inception.caffemodel http://mmlab.siat.ac.cn/hmdb51_split_1_tsn_rgb_reference_bn_inception.caffemodel.v5
-wget -O models/hmdb51_split_2_tsn_rgb_reference_bn_inception.caffemodel http://mmlab.siat.ac.cn/hmdb51_split_2_tsn_rgb_reference_bn_inception.caffemodel.v5
-wget -O models/hmdb51_split_3_tsn_rgb_reference_bn_inception.caffemodel http://mmlab.siat.ac.cn/hmdb51_split_3_tsn_rgb_reference_bn_inception.caffemodel.v5
+wget -O models/hmdb51_split_1_tsn_rgb_reference_bn_inception.caffemodel http://mmlab.siat.ac.cn/tsn_model/hmdb51_split_1_tsn_rgb_reference_bn_inception.caffemodel.v5
+wget -O models/hmdb51_split_2_tsn_rgb_reference_bn_inception.caffemodel http://mmlab.siat.ac.cn/tsn_model/hmdb51_split_2_tsn_rgb_reference_bn_inception.caffemodel.v5
+wget -O models/hmdb51_split_3_tsn_rgb_reference_bn_inception.caffemodel http://mmlab.siat.ac.cn/tsn_model/hmdb51_split_3_tsn_rgb_reference_bn_inception.caffemodel.v5
 
 # hmdb51 flow models, 3 splits
 wget -O models/hmdb51_split_1_tsn_flow_reference_bn_inception.caffemodel http://mmlab.siat.ac.cn/tsn_model/hmdb51_split_1_tsn_flow_reference_bn_inception.caffemodel.v5
