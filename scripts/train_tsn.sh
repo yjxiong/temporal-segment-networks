0eb1040c5895522403109921edb3be25b4fb716f
yjxiong <xy012@ie.cuhk.edu.hk>
2016-09-01 20:34:27 +0800

    minor fix

--- scripts/train_tsn.sh
+++ scripts/train_tsn.sh
@@ -6,11 +6,11 @@ MODALITY=$2
 TOOLS=lib/caffe-action/build/install/bin
 LOG_FILE=logs/${DATASET}_${MODALITY}_split1.log
 N_GPU=4
-MPI_BIN_DIR= #/usr/local/openmpi/bin
+MPI_BIN_DIR= #/usr/local/openmpi/bin/
 
 
 echo "logging to ${LOG_FILE}"
 
-${MPI_BIN_DIR}/mpirun -np $N_GPU \
+${MPI_BIN_DIR}mpirun -np $N_GPU \
 $TOOLS/caffe train --solver=models/${DATASET}/tsn_bn_inception_${MODALITY}_solver.prototxt  \
    --weights=models/bn_inception_${MODALITY}_init.caffemodel 2>&1 | tee ${LOG_FILE}
