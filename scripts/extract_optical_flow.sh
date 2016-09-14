e063db97ff96d9bfa80bc4c412e057332e122a05
yjxiong <yjxiong@users.noreply.github.com>
2016-09-14 11:54:54 +0800

    set frame sizes in flow extraction

--- scripts/extract_optical_flow.sh
+++ scripts/extract_optical_flow.sh
@@ -5,4 +5,4 @@ OUT_FOLDER=$2
 NUM_WORKER=$3
 
 echo "Extracting optical flow from videos in folder: ${SRC_FOLDER}"
-python tools/build_of.py ${SRC_FOLDER} ${OUT_FOLDER} --num_worker ${NUM_WORKER} 2>local/errors.log
+python tools/build_of.py ${SRC_FOLDER} ${OUT_FOLDER} --num_worker ${NUM_WORKER} --new_width 340 --new_height 256 2>local/errors.log
