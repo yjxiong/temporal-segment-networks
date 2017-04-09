b1251e8a5fd1b0a6abdec7519aa8ec5ba9ab7ae6
vra <wangyf11@mail.ustc.edu.cn>
2017-04-09 21:08:09 +0800

    fix prefix and typo

--- tools/build_file_list.py
+++ tools/build_file_list.py
@@ -10,8 +10,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('dataset', type=str, choices=['ucf101', 'hmdb51', 'activitynet_1.2', 'activitynet_1.3'])
 parser.add_argument('frame_path', type=str, help="root directory holding the frames")
 parser.add_argument('--rgb_prefix', type=str, help="prefix of RGB frames", default='img_')
-parser.add_argument('--flow_x_prefix', type=str, help="prefix of x direction flow images", default='flow_x')
-parser.add_argument('--flow_y_prefix', type=str, help="prefix of y direction flow images", default='flow_y')
+parser.add_argument('--flow_x_prefix', type=str, help="prefix of x direction flow images", default='flow_x_')
+parser.add_argument('--flow_y_prefix', type=str, help="prefix of y direction flow images", default='flow_y_')
 parser.add_argument('--num_split', type=int, default=3)
 parser.add_argument('--out_list_path', type=str, default='data/')
 parser.add_argument('--shuffle', action='store_true', default=False)
@@ -33,7 +33,7 @@ print 'processing dataset {}'.format(dataset)
 split_tp = parse_split_file(dataset)
 f_info = parse_directory(frame_path, rgb_p, flow_x_p, flow_y_p)
 
-print 'writting list files for training/testing'
+print 'writing list files for training/testing'
 for i in xrange(max(num_split, len(split_tp))):
     lists = build_split_list(split_tp, f_info, i, shuffle)
     open(os.path.join(out_path, '{}_rgb_train_split_{}.txt'.format(dataset, i+1)), 'w').writelines(lists[0][0])
