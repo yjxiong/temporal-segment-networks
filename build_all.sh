8a0f4e6c53b538c931d6e420929d6c14de092246
yjxiong <yjxiong@users.noreply.github.com>
2017-06-20 15:39:42 +0800

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -66,9 +66,9 @@ cd ../../caffe-action
 [[ -d build ]] || mkdir build
 cd build
 if [ "$CAFFE_USE_MPI" == "MPI_ON" ]; then
-OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DUSE_MPI=ON -DMPI_CXX_COMPILER="${CAFFE_MPI_PREFIX}/bin/mpicxx"
+OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DUSE_MPI=ON -DMPI_CXX_COMPILER="${CAFFE_MPI_PREFIX}/bin/mpicxx" -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
 else
-OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake ..
+OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
 fi
 if make -j32 install ; then
     echo "Caffe Built."
