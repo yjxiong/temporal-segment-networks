05db45377b83c4ee5937e35dc5f215c7eab48a3a
yjxiong <yjxiong@users.noreply.github.com>
2017-08-10 19:18:30 +0800

    remove xine dependency

--- build_all.sh
+++ build_all.sh
@@ -23,7 +23,7 @@ cd 3rd-party/
 
 if [ ! -d "opencv-$version" ]; then
     echo "Installing OpenCV Dependenices"
-    sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
+    sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
 
     echo "Downloading OpenCV" $version
     wget -O OpenCV-$version.zip https://github.com/Itseez/opencv/archive/$version.zip
