70af3eaea3de704c91b2be8f068cb9205cf0d890
yjxiong <yjxiong@users.noreply.github.com>
2017-11-06 02:33:41 +0800

    move init models

--- scripts/get_init_models.sh
+++ scripts/get_init_models.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget -O models/bn_inception_rgb_init.caffemodel http://mmlab.siat.ac.cn/tsn_model/bn_inception_spatial_var_init.caffemodel
-wget -O models/bn_inception_flow_init.caffemodel http://mmlab.siat.ac.cn/tsn_model/bn_inception_flow_var_init.caffemodel
+wget -O models/bn_inception_rgb_init.caffemodel https://yjxiong.blob.core.windows.net/tsn-init/bn_inception_rgb_init.caffemodel
+wget -O models/bn_inception_flow_init.caffemodel https://yjxiong.blob.core.windows.net/tsn-init/bn_inception_flow_init.caffemodel
